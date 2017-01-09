97041664e7410f245586cc4ec281dceca1f962f5
MeteorSis <uncleone@naver.com>
2017-01-09 17:45:20 +0900

    add menu()

--- OOP_Project/main.cpp
+++ OOP_Project/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
+void menu(void);
+
 int main(void)
 {
+	menu();
 	return 0;
+}
+
+void menu(void)
+{
+	cout << "-----Menu------" << endl;
+	cout << "1. 계좌개설" << endl;
+	cout << "2. 입 금" << endl;
+	cout << "3. 출 금" << endl;
+	cout << "4. 계좌정보 전체 출력" << endl;
+	cout << "5. 프로그램 종료" << endl;
+	cout << "선택 : ";
 }
