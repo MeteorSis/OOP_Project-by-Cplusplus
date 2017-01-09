2e251416e95305d2f5db5325ceebf3af8375a6ed
MeteorSis <uncleone@naver.com>
2017-01-09 17:53:01 +0900

    add loop in menu()

--- OOP_Project/main.cpp
+++ OOP_Project/main.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
 using namespace std;
 
-void menu(void);
+int menu(void);
 
 int main(void)
 {
-	menu();
+	
 	return 0;
 }
 
-void menu(void)
+int menu(void)
 {
-	cout << "-----Menu------" << endl;
-	cout << "1. 계좌개설" << endl;
-	cout << "2. 입 금" << endl;
-	cout << "3. 출 금" << endl;
-	cout << "4. 계좌정보 전체 출력" << endl;
-	cout << "5. 프로그램 종료" << endl;
-	cout << "선택 : ";
+	int choice;
+	while (true)
+	{
+		cout << "-----Menu------" << endl;
+		cout << "1. 계좌개설" << endl;
+		cout << "2. 입 금" << endl;
+		cout << "3. 출 금" << endl;
+		cout << "4. 계좌정보 전체 출력" << endl;
+		cout << "5. 프로그램 종료" << endl;
+		cout << "선택 : ";
+		cin >> choice;
+		if (choice >= 1 && choice <= 5)
+			break;
+		cout << "\n값이 잘못되었습니다.\n다시 입력해주십시오\n" << endl;
+	}
+	return choice;
 }
